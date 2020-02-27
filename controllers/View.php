c9b3735a9a85399ca14b9aabdde3f7625c0e9dde
Navneet Singh <navneetofficial25@gmail.com>
2020-02-27 22:50:54 +0530

    View part

---
+++ controllers/View.php
@@ -0,0 +1,16 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class View extends CI_Controller {
+
+    public function index()
+    {
+        $this->load->view('index');
+        
+    }
+
+}
+
+/* End of file View.php */
---
+++ views/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <p class="h2">Welcome To enaicoder</p>
+    </div>
+</body>
+</html>
